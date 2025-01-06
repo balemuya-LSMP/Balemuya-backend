16660dbfa55071dd1ce8d60a8a5a75562562cc4c
= <=>
2025-01-06 14:58:54 +0300

    created user and address admin in admin.py in  users app

--- Balemuya/users/admin.py
+++ Balemuya/users/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import User, Address
 
-# Register your models here.
+class AddressInline(admin.StackedInline):
+    model = Address
+    extra = 1 
+    fields = ['country', 'region', 'woreda', 'city', 'kebele', 'street', 'latitude', 'longitude', 'is_current']  
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = [
+        'email', 'first_name', 'middle_name', 'last_name',
+        'phone_number', 'gender', 'profile_image', 'kebele_id_image',
+        'user_type', 'is_active', 'is_superuser'
+    ]
+    list_filter = [
+        'user_type', 'email', 'first_name', 'middle_name',
+        'last_name', 'phone_number', 'gender', 'is_active'
+    ]
+    search_fields = [
+        'email', 'first_name', 'middle_name', 'last_name',
+        'phone_number', 'gender'
+    ]
+    inlines = [AddressInline] 
+
+class AddressAdmin(admin.ModelAdmin):
+    model = Address
+    list_display = [
+        'user', 'country', 'region', 'woreda', 
+        'city', 'kebele', 'street', 'latitude', 
+        'longitude', 'is_current'
+    ]
+    search_fields = [
+        'country', 'region', 'woreda', 'city', 
+        'kebele', 'street'
+    ]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Address, AddressAdmin)
