927d16dc5f9bfe2dd7ff591bf067ba83304c0092
Yikeber <yikeber50@gmail.com>
2025-01-08 13:28:14 +0300

    updated models

--- Balemuya/users/views.py
+++ Balemuya/users/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render,HttpResponse
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProfessionalProfileSerializer
 
-# Create your views here.
-def home(request):
-    return HttpResponse('hello world')
+class ProfessionalRegisterView(generics.CreateAPIView):
+    pass
+    # serializer_class = ProfessionalSerializer
+
+    # def post(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     user = self.perform_create(serializer)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # def perform_create(self, serializer):
+    #     return serializer.save()
