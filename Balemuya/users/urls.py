ff0c13ae43463b3778ef7decf1b282ae246dddbe
Yikeber <yikeber50@gmail.com>
2025-01-10 02:08:38 +0300

    confirm the user via sms using twilio test trial

--- Balemuya/users/urls.py
+++ Balemuya/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import RegisterView
 from allauth.account.views import ConfirmEmailView
+from .utils import send_sms
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
+    path('auth/call/', send_sms, name='send-sms'),
     path('auth/confirm-email/<str:key>/', ConfirmEmailView.as_view(), name='account_confirm_email'),  # Updated
 ]
