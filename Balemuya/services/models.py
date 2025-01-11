e8394f051ce5950d7d6f24ddaad56e5849d91aad
Yikeber <yikeber50@gmail.com>
2025-01-11 20:57:58 +0300

    add services app in settings and create category model on services

--- Balemuya/services/models.py
+++ Balemuya/services/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank = True,null = True)
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
