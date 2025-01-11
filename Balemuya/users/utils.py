5928accc349b882fde03b6d52ba7b1085fa6449d
Yikeber <yikebermisganaw@gmail.com>
2025-01-12 01:27:20 +0300

    update email and sms verification account

--- Balemuya/users/utils.py
+++ Balemuya/users/utils.py
@@ -4,13 +4,14 @@ from django_twilio.client import twilio_client
 from django.http import JsonResponse
 import random
 
+
 @staticmethod
 def generate_otp():
-        """Generates a 6-digit random OTP."""
-        return random.randint(100000, 999999)
-    
+    """Generates a 6-digit random OTP."""
+    return random.randint(100000, 999999)
 
-def send_sms(request,to,message_body):
+
+def send_sms(request, to, message_body):
     try:
         sender_number = settings.TWILIO_DEFAULT_CALLERID
         if not sender_number:
@@ -21,21 +22,23 @@ def send_sms(request,to,message_body):
             from_=sender_number,
             to=to
         )
-        print('message is sent ',message,'to',to,'from','sender',sender_number)
+        print('message is sent ', message, 'to', to, 'from', 'sender', sender_number)
         return JsonResponse({"message": f"Message sent: {message.sid}"})
     except Exception as e:
         return JsonResponse({"error": str(e)})
-    
 
-def send_email_confirmation(subject, message,recipient_list):
+
+def send_email_confirmation(subject, message, recipient_list):
     try:
         response = send_mail(
             subject,
             message,
             settings.EMAIL_HOST_USER,
-            recipient_list)
+            recipient_list,
+            fail_silently=False
+        )
+        print(f"Email sent response: {response}")
         return response
     except Exception as e:
+        print(f"Error sending email: {str(e)}")
         return str(e)
-    
-    
