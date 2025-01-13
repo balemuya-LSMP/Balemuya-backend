399852f32f413456e70a585a06b1ffda07f82eb5
Yikeber <yikebermisganaw@gmail.com>
2025-01-13 16:42:06 +0300

    chage python to 3.9 and django version to 4.2.1

--- Balemuya/build_files.sh
+++ Balemuya/build_files.sh
@@ -5,6 +5,6 @@ echo "BUILD START"
 python3.9 -m pip install --upgrade pip  
 python3.9 -m pip install -r requirements.txt  
 python3.9 manage.py collectstatic --noinput --clear 
-python3.9 manage.py migrate --noinput  # Run migrations
+python3.9 manage.py migrate --noinput 
 
 echo "BUILD END"
