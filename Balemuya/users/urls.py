82624fae9be446edbb99b98bb9b57898689b26aa
Yikeber <yikebermisganaw@gmail.com>
2025-01-14 23:25:04 +0300

    password reset and password reset otp is done

--- Balemuya/users/urls.py
+++ Balemuya/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView,LoginView,VerifyEmailView,LogoutView,VerifyOTPView,ResendOTPView,SetPasswordView,ResetPasswordView
+from .views import RegisterView,LoginView,VerifyEmailView,LogoutView,VerifyPhoneView,VerifyPaswordResetOTPView,ResendOTPView,SetPasswordView,ResetPasswordView
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
@@ -8,9 +8,10 @@ urlpatterns = [
     path('auth/login/',LoginView.as_view(), name='user-login'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/logout/',LogoutView.as_view(), name='user-logout'),
-    path('auth/verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
+    path('auth/verify-phone/', VerifyPhoneView.as_view(), name='verify-phone'),
     path('auth/resend-otp/', ResendOTPView.as_view(), name='resend-otp'),
-    path('auth/set-password/', SetPasswordView.as_view(), name='set-password'),
     path('auth/reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    path('auth/verify-pass-reset-otp/', VerifyPaswordResetOTPView.as_view(), name='verify-otp'),
+    path('auth/set-password/', SetPasswordView.as_view(), name='set-password'),
       
 ]
