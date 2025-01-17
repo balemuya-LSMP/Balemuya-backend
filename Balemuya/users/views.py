62996e5a150776a8ffee9064a8657b55a7f0d72e
Yikeber <yikeber50@gmail.com>
2025-01-17 20:39:03 +0300

    update usersList view

--- Balemuya/users/views.py
+++ Balemuya/users/views.py
@@ -326,8 +326,7 @@ class ProfileView(APIView):
         user = request.user
         if user is None:
             return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-        if user.user_type == 'customer':
-            
+        if user.user_type == 'customer': 
             customer = CustomerProfile.objects.filter(user=user).first()
             if customer is None:
                 return Response({'error': 'Customer not found.'}, status=status.HTTP_404_NOT_FOUND)
@@ -472,13 +471,40 @@ class ProfessionalListView(generics.ListAPIView):
                 queryset = queryset.filter(user__is_blocked=True)
 
         return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if not queryset.exists():
+            return Response({"message": "No users found matching the criteria."}, status=404)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
 # View for listing Customers
 class CustomerListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = CustomerProfileSerializer
 
     def get_queryset(self):
-        return CustomerProfile.objects.filter(user__user_type='customer',user__is_active=True)
+        queryset = CustomerProfile.objects.all()
+        status_filter = self.request.query_params.get('status', None)
+
+        if status_filter:
+            if status_filter == 'active':
+                queryset = queryset.filter(user__is_active=True)
+            elif status_filter == 'blocked':
+                queryset = queryset.filter(user__is_blocked=True)
+
+        return queryset
+
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if not queryset.exists():
+            return Response({"message": "No customers found."}, status=404)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 # View for listing Admins
 class AdminListView(generics.ListAPIView):
