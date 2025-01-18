e89b739f21d589c4d1b2f6cadcfd9112c5be03b9
Yikeber <yikebermisganaw@gmail.com>
2025-01-18 14:11:45 +0300

    create new db structure

--- Balemuya/services/admin.py
+++ Balemuya/services/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'id')
+    search_fields = ('name', 'description')
+    list_filter = ('name',)
+    ordering = ('name',)
+    list_per_page = 10
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description')
+        }),
+    )
+
+admin.site.register(Category, CategoryAdmin)
