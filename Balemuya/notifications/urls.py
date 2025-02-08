a0da61c4f577c1c298b1751af26fdf9360eb66c3
Yikeber <yikebermisganaw@gmail.com>
2025-02-08 14:39:20 +0300

    add all notification  as read

--- Balemuya/notifications/urls.py
+++ Balemuya/notifications/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import NotificationListView,MarkNotificationAsReadView
+from .views import NotificationListView,MarkNotificationAsReadView,MarkAllNotificationAsReadView
 
 
 urlpatterns = [
     path('', NotificationListView.as_view(), name='notifications-list'),
     path('<uuid:pk>/read/', MarkNotificationAsReadView.as_view(), name='notification-mark-read'),
+    path('read/', MarkAllNotificationAsReadView.as_view(), name='notification-mark-read'),
 
 ]
