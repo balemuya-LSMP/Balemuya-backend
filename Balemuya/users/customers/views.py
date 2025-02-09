331db75ee7e8b55233bac0227d1c15926ace685a
Yikeber <yikeber50@gmail.com>
2025-02-09 10:29:26 +0300

    calculate nearby professionals

--- Balemuya/users/customers/views.py
+++ Balemuya/users/customers/views.py
@@ -8,33 +8,37 @@ from users.serializers import ProfessionalSerializer
 
 class NearbyProfessionalsView(APIView):
     def get(self, request):
-        # Get the customer's location from the user's address
+        category = request.query_params.get('category')
         customer_lat = request.user.address.latitude
         customer_lon = request.user.address.longitude
         customer_location = (customer_lat, customer_lon)
 
-        nearby_professionals = self.find_nearby_professionals(customer_location)
+        nearby_professionals = self.find_nearby_professionals(customer_location,category,max_distance=10)
+        if not nearby_professionals:
+            return Response({"message": "No professionals found nearby"}, status=status.HTTP_404_NOT_FOUND)
 
         return Response({"message": "success", "nearby_professionals": nearby_professionals}, status=status.HTTP_200_OK)
 
-    def find_nearby_professionals(self, customer_location):
+    def find_nearby_professionals(self, customer_location, category=None, max_distance=None):
         nearby = []
 
-        professionals = User.objects.filter(user_type='professional')
+        professionals = User.objects.filter(user_type='professional', professional__categories__name=category)
 
         for professional in professionals:
-            # Ensure the professional has an address
             if professional.address:
                 professional_location = (professional.address.latitude, professional.address.longitude)
                 distance = geodesic(customer_location, professional_location).kilometers
-                nearby.append({
-                    "id": professional.id,
-                    "name": professional.first_name,
-                    "user_type": professional.user_type,
-                    "latitude": professional.address.latitude,
-                    "longitude": professional.address.longitude,
-                    "distance": round(distance, 2)
-                })
+                
+                # Check if the distance is within the specified max_distance
+                if max_distance is None or distance <= max_distance:
+                    nearby.append({
+                        "id": professional.id,
+                        "name": professional.first_name,
+                        "user_type": professional.user_type,
+                        "latitude": professional.address.latitude,
+                        "longitude": professional.address.longitude,
+                        "distance": round(distance, 2)
+                    })
 
         nearby.sort(key=lambda x: x['distance'])
         return nearby
