b8ad7019fe3924289f30cf762f3ea1b361846f77
= <=>
2025-02-09 14:19:05 +0300

    create profile for customer and professional

---
+++ Balemuya/users/professional/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path 
+import uuid
+from .views import ProfessionalProfileView, ProfessionalProfileUpdateView,ProfessionalSkillView,ProfessionalCategoryView,CertificateView,EducationView,PortfolioView,\
+    ProfessionalVerificationRequestView,InitiatePaymentView,CheckPaymentView,ProfessionalSubscriptionHistoryView
+
+urlpatterns = [
+    path('<uuid:pk>/profile/',ProfessionalProfileView.as_view(), name='professional-profile'),
+    path('profile/update/',ProfessionalProfileUpdateView.as_view(), name='professional-update'),
+    path('profile/skills/', ProfessionalSkillView.as_view(), name='skill-create-update-delete'),
+    path('profile/categories/', ProfessionalCategoryView.as_view(), name='category-create-delete'),
+    
+    path('profile/certificates/add/', CertificateView.as_view(), name='certificate-create'),  # POST
+    path('profile/certificates/<uuid:pk>/update/', CertificateView.as_view(), name='certificate-update'),  # PUT
+    path('profile/certificates/<uuid:pk>/delete/', CertificateView.as_view(), name='certificate-delete'),  # DELETE
+    
+    path('profile/educations/add/', EducationView.as_view(), name='education-create'),  # POST
+    path('profile/educations/<uuid:pk>/update/', EducationView.as_view(), name='education-update'),  # PUT
+    path('profile/educations/<uuid:pk>/delete/', EducationView.as_view(), name='education-delete'),  # DELETE
+    
+    path('profile/portfolios/add/', PortfolioView.as_view(), name='portfolio-create'),  # POST
+    path('profile/portfolios/<uuid:pk>/update/', PortfolioView.as_view(), name='portfolio-update'),  # PUT
+    path('profile/portfolios/<uuid:pk>/delete/', PortfolioView.as_view(), name='portfolio-delete'),  # DELETE
+    path('verification-requests/', ProfessionalVerificationRequestView.as_view(), name='professional-verification-request'),
+    path('subscription/history/',ProfessionalSubscriptionHistoryView.as_view(), name='professional-subscription-history'),
+
+    #payment related
+    path('subscription/payment/initiate/', InitiatePaymentView.as_view(), name='initiate_payment'),
+    path('subscription/payment/check/<str:transaction_id>/', CheckPaymentView.as_view(), name='check_payment'),
+]
