fd3c71afb34bac3fe7565c6db552b0af87dd2bd4
Yikeber <yikeber50@gmail.com>
2025-02-09 16:47:12 +0300

    udate proximity distance

--- Balemuya/users/customer/urls.py
+++ Balemuya/users/customer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 import uuid
-from .views import CustomerProfileView,NearbyProfessionalsView
+from .views import CustomerProfileView,CustomerServicesView,NearbyProfessionalsView
 
 urlpatterns = [
     path('<uuid:pk>/profile/',CustomerProfileView.as_view(), name='customer-profile'),
     path('nearby-professionals/',NearbyProfessionalsView.as_view(), name='nearby-professionals'),
+    
+    path('services/',CustomerServicesView.as_view(), name='customer-services'),
 ]
