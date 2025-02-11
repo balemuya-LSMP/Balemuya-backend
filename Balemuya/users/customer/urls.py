a015e5a444453e01abab0a306a8a4034c88b8a3d
Yikeber <yikebermisganaw@gmail.com>
2025-02-11 14:25:04 +0300

    created service request view

--- Balemuya/users/customer/urls.py
+++ Balemuya/users/customer/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 import uuid
-from .views import CustomerProfileView,CustomerServicesView,NearbyProfessionalsView,FilterProfessionalsView
+from .views import CustomerProfileView,CustomerServicesView,NearbyProfessionalsView,FilterProfessionalsView,CustomerServiceRequestAPIView,CancelServiceRequestAPIView
 
 urlpatterns = [
     path('<uuid:pk>/profile/',CustomerProfileView.as_view(), name='customer-profile'),
     path('nearby-professionals/',NearbyProfessionalsView.as_view(), name='nearby-professionals'),
     path('filter-professionals/',FilterProfessionalsView.as_view(), name='filter-professionals'),
     
+    path('service-request/', CustomerServiceRequestAPIView.as_view(), name='create_service_request'),  
+    path('service-request/<int:request_id>/cancel/', CancelServiceRequestAPIView.as_view(), name='cancel_service_request'),
+      
     path('services/',CustomerServicesView.as_view(), name='customer-services'),
 ]
