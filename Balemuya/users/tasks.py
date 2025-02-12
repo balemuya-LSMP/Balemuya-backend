0ba9c849712150b2e2a76c5267f66c3134b1d411
Yikeber <yikebermisganaw@gmail.com>
2025-02-12 16:43:49 +0300

    updated servise requests view on professional

--- Balemuya/users/tasks.py
+++ Balemuya/users/tasks.py
@@ -1,36 +1,36 @@
-# users/tasks.py
-from celery import shared_task
-from django.utils import timezone
-from .models import SubscriptionPlan, Professional
-from django.core.mail import send_mail
-from django.conf import settings
+# # users/tasks.py
+# from celery import shared_task
+# from django.utils import timezone
+# from .models import SubscriptionPlan, Professional
+# from django.core.mail import send_mail
+# from django.conf import settings
 
-@shared_task
-def notify_expiring_subscriptions():
-    current_date = timezone.now()
-    expiration_threshold = current_date + timedelta(days=7)
+# @shared_task
+# def notify_expiring_subscriptions():
+#     current_date = timezone.now()
+#     expiration_threshold = current_date + timedelta(days=7)
 
-    expiring_subscriptions = SubscriptionPlan.objects.filter(
-        expiration_date__lte=expiration_threshold,
-        is_expired=False
-    )
+#     expiring_subscriptions = SubscriptionPlan.objects.filter(
+#         expiration_date__lte=expiration_threshold,
+#         is_expired=False
+#     )
 
-    for subscription in expiring_subscriptions:
-        try:
-            professional = Professional.objects.get(id=subscription.professional.id)
-            professional.is_available = False
-            professional.save()
-            send_notification_email(subscription.user.email)
-        except Professional.DoesNotExist:
-            continue
+#     for subscription in expiring_subscriptions:
+#         try:
+#             professional = Professional.objects.get(id=subscription.professional.id)
+#             professional.is_available = False
+#             professional.save()
+#             send_notification_email(subscription.user.email)
+#         except Professional.DoesNotExist:
+#             continue
 
-@shared_task
-def send_notification_email(user_email):
-    if user_email:
-        send_mail(
-            'Your account subscription is expiring soon',
-            'Your subscription is expiring soon. Please renew it.',
-            settings.DEFAULT_FROM_EMAIL,
-            [user_email],
-            fail_silently=False,
-        )
+# @shared_task
+# def send_notification_email(user_email):
+#     if user_email:
+#         send_mail(
+#             'Your account subscription is expiring soon',
+#             'Your subscription is expiring soon. Please renew it.',
+#             settings.DEFAULT_FROM_EMAIL,
+#             [user_email],
+#             fail_silently=False,
+#         )
