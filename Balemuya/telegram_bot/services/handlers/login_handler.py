7e2cf4cb14e63cb3c9dd43fef908e94056811225
Yikeber <yikeber50@gmail.com>
2025-04-30 22:50:37 +0300

    updated

---
+++ Balemuya/telegram_bot/services/handlers/login_handler.py
@@ -0,0 +1,32 @@
+# services/handlers/login_handler.py
+
+class LoginHandler:
+    def __init__(self, facade):
+        self.facade = facade
+
+    def handle(self, text, user_state):
+        chat_id = self.facade.chat_id
+        auth = self.facade.auth_service
+        bot = self.facade.bot_service
+
+        if text == "🔐 Login":
+            self.facade.ask_for_email()
+            auth.set_user_state("waiting_for_login_email")
+
+        elif user_state == "waiting_for_login_email":
+            auth.set_session_data("email", text.strip())
+            auth.set_user_state("waiting_for_login_password")
+            bot.send_message(chat_id, "🔑 Please enter your password:")
+
+        elif user_state == "waiting_for_login_password":
+            email = auth.get_session_data("email")
+            password = text.strip()
+            response = auth.send_login_request(email, password)
+
+            if response.get("status") == "success":
+                self.facade.send_login_success()
+            else:
+                self.facade.send_login_failure(response.get('error'))
+                self.send_main_menu(message='please try to login again!')
+
+            auth.clear_session()
