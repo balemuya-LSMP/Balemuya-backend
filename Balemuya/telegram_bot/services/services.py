e11b34bd73f7655ebc72439dc1da0b759c9b55c5
= <=>
2025-04-30 21:23:47 +0300

    registration and login of bot completed

--- Balemuya/telegram_bot/services/services.py
+++ Balemuya/telegram_bot/services/services.py
@@ -33,6 +33,8 @@ class TelegramAuthService:
         try:
             url = f"{settings.BACKEND_URL}users/auth/register/"
             response = requests.post(url, data=data)
+            print('registration status code',response.status_code)
+            print('registration status data',response.json())
             if response.status_code == 201:
                 return {"status": "success"}
             return {"status": "failure", "message": response.text}
@@ -44,10 +46,12 @@ class TelegramAuthService:
             url = f"{settings.BACKEND_URL}users/auth/login/"
             response = requests.post(url, data={"email": email, "password": password})
             print('login response is ',response)
+            print('login response status code ',response.status_code)
+            print('login response data is ',response.json())
             if response.status_code == 200:
                 data = response.json()
-                access_token = data.get("access")
-                refresh_token = data.get("refresh")
+                access_token = data.get("user")['access']
+                refresh_token = data.get("user")['refresh']
                 if access_token and refresh_token:
                     cache.set(f"user_access_token_{self.chat_id}", access_token, timeout=3600)  # usually short-lived
                     cache.set(f"user_refresh_token_{self.chat_id}", refresh_token, timeout=7 * 24 * 3600)  # longer-lived
