52a9cfbcea4f5a0e55740c5dd29c201fc090649a
Yikeber <yikeber50@gmail.com>
2025-05-22 13:08:45 +0300

    update bot

--- Balemuya/telegram_bot/utils/common.py
+++ Balemuya/telegram_bot/utils/common.py
@@ -3,22 +3,16 @@ import pytz
 from PIL import Image, ImageDraw
 import requests
 
-def create_circular_image(image_path, output_path):
+
+def create_circular_image(image, size=(100, 100)):
     """Create a circular image from the original image."""
-    img = Image.open(image_path)
-    img = img.resize((100, 100))  # Resize to desired size
-    
-    # Create a new image with a transparent background
-    circle = Image.new('L', (100, 100), 0)
+    img = image.resize(size, Image.ANTIALIAS)  # Resize to desired size
+    circle = Image.new('L', size, 0)
     draw = ImageDraw.Draw(circle)
-    draw.ellipse((0, 0, 100, 100), fill=255)
-    
-    # Create a mask
-    mask = Image.new('L', (100, 100), 0)
+    draw.ellipse((0, 0, size[0], size[1]), fill=255)
+
+    mask = Image.new('L', size, 0)
     mask.paste(circle, (0, 0))
-
-    # Apply the mask to the image
     img.putalpha(mask)
-
-    # Save the circular image
-    img.save(output_path)
+    
+    return img
