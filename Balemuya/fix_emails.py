f3bed60ab1af5116941e0fd51b02370f414b0b88
Yikeber <yikebermisganaw@gmail.com>
2025-06-04 13:58:58 +0300

    delete fix email

--- Balemuya/fix_emails.py
+++
@@ -1,7 +0,0 @@
-def commit_callback(commit):
-    if commit.author_email == b"":
-        commit.author_email = b"yikeber50@gmail.com"
-        commit.author_name = b"Yikeber"
-    if commit.committer_email == b"":
-        commit.committer_email = b"yikeber50@gmail.com"
-        commit.committer_name = b"Yikeber"
